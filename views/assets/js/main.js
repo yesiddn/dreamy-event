3c82b7ae0546a03e4fc686f9c655d36affdb3565
Yesid <yesidrodriguez305@gmail.com>
2023-10-25 11:54:41 -0500

    fix(app): Fix sign up form and control query

--- views/assets/js/main.js
+++ views/assets/js/main.js
@@ -5,11 +5,10 @@ customerFormButton.addEventListener('click', createCustomer);
 async function fetchData(url, method, data) {
   const response = await fetch(url, {
     method,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data),
+    body: data,
   });
+
+  return await response.json();
 }
 
 function getFormCustomerData() {
@@ -21,23 +20,25 @@ function getFormCustomerData() {
 
 function validateFormData(formData) {
   let isValid = true;
-  
+
   for (const [key, value] of formData.entries()) {
     isValid = validateInputData(key) && isValid;
   }
 
-  return {isValid, formData};
+  return { isValid, formData };
 }
 
 function validateInputData(inputId) {
   const input = document.getElementById(inputId);
   const invalidFeedback = input.nextElementSibling;
-  if (input.value.trim() === '' || input.validity.valida === false) {
+  if (
+    input.value.trim() === '' ||
+    (input.validity.typeMismatch === false) === false
+  ) {
     input.classList.add('is-invalid');
 
     // activate the invalid feedback
     invalidFeedback.classList.remove('inactive');
-    // invalidFeedback.style.display = 'block';
 
     return false;
   } else {
@@ -47,18 +48,20 @@ function validateInputData(inputId) {
   }
 }
 
-function createCustomer() {
+async function createCustomer() {
   const { isValid, formData } = getFormCustomerData();
   if (!isValid) {
     return false;
   }
-  
-  const url = 'control/customer.control.php';
-  const method = 'POST';
-  const data = {
-    action: 'create',
-    formData
-  };
 
-  const response = fetchData(url, method, data);
+  const url = '../../control/customer.control.php';
+  const method = 'POST';
+  formData.set('action', 'create');
+
+  try {
+    const response = await fetchData(url, method, formData);
+    console.log(response);
+  } catch (error) {
+    console.log(error); 
+  }
 }
