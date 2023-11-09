e5b0c578bfc72d481216d2cd736720595e2e0426
Duvan Yesid <63770233+yesiddn@users.noreply.github.com>
2023-11-09 08:03:58 -0500

    Update head.php

--- views/modules/head.php
+++ views/modules/head.php
@@ -6,5 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="./views/assets/css/normalize.css">
   <link rel="stylesheet" href="./views/assets/css/main.css">
+  <script src="./views/assets/js/main.js"></script>
   <title>Dreamy Event</title>
 </head>
