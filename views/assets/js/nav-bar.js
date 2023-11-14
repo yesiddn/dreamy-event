f272f508fe411082847c39a9ecafadf6dc39f13a
Yesid <yesidrodriguez305@gmail.com>
2023-11-14 15:07:15 -0500

    feat(app): Add profile image to navbar

--- views/assets/js/nav-bar.js
+++ views/assets/js/nav-bar.js
@@ -1,5 +1,11 @@
 const hamburguerMenu = document.querySelector('.hamburguer-menu');
 const menu = document.querySelector('.nav-bar div');
+const profileImg = document.querySelector('#user-profile-img');
+
+const user = JSON.parse(localStorage.getItem('user'));
+if (user) {
+  profileImg.style.backgroundImage = `url(${user.img_user})`;
+}
 
 hamburguerMenu.addEventListener('click', (e) => {
   const isClickInsideButton = hamburguerMenu.contains(e.target) || e.target === hamburguerMenu;
