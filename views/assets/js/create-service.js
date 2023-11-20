985f1317a68a56982037d27243369648cad84c49
AlejoShox <pedroalcaro@live.com>
2023-11-20 11:03:18 -0500

    starting the JS file

---
+++ views/assets/js/create-service.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+
+
+
+document.querySelector('#service-form-button').addEventListener('click',function(){
+
+    let nameService = document.querySelector('#name-service').value;
+    let serviceDescript = document.querySelector('#service-description').value;
+    let priceService = document.querySelector('#price-service').value;
+    let locationService = document.querySelector('#location-service').value;
+    let cityService = document.querySelector('#city-service').value;
+    let countryService = document.querySelector('#country-service').value;
+    let peopleAmountService = document.querySelector('#peopleAmount-service').value;
+
+
+
+    alert('hola');
+
+
+});
+
+
+
+
+
+})
