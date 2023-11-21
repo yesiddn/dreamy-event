eaba4c7c720e904335b948eaae3a8edbe45ebd1e
AlejoShox <pedroalcaro@live.com>
2023-11-20 22:43:39 -0500

    Js almost finished with a fetch template

--- views/assets/js/create-service.js
+++ views/assets/js/create-service.js
@@ -1,25 +1,44 @@
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener('DOMContentLoaded', function () {
 
 
 
+    document.querySelector('#service-form-button').addEventListener('click', function () {
 
-document.querySelector('#service-form-button').addEventListener('click',function(){
+        let nameService = document.querySelector('#name-service').value;
+        let serviceDescript = document.querySelector('#description-service').value;
+        let priceService = document.querySelector('#price-service').value;
+        let locationService = document.querySelector('#location-service').value;
+        let cityService = document.querySelector('#city-service').value;
+        let countryService = document.querySelector('#country-service').value;
+        let peopleAmountService = document.querySelector('#peopleAmount-service').value;
+        let characteristicsSservice = document.querySelector('#characteristics-service').value;
 
-    let nameService = document.querySelector('#name-service').value;
-    let serviceDescript = document.querySelector('#service-description').value;
-    let priceService = document.querySelector('#price-service').value;
-    let locationService = document.querySelector('#location-service').value;
-    let cityService = document.querySelector('#city-service').value;
-    let countryService = document.querySelector('#country-service').value;
-    let peopleAmountService = document.querySelector('#peopleAmount-service').value;
+        // Crear un objeto FormData y agregar las variables
+        var formData = new FormData();
+        formData.append('nameService', nameService);
+        formData.append('descriptionService', serviceDescript);
+        formData.append('price', priceService);
+        formData.append('location', locationService);
+        formData.append('city', cityService);
+        formData.append('country', countryService);
+        formData.append('amountPeople', peopleAmountService);
+        formData.append('characteristics', characteristicsSservice);
+        formData.append('queryType','Insert')
+        
 
-
-
-    alert('hola');
-
-
-});
-
+        // Realizar la solicitud fetch
+        fetch('../assets/js/services.control.php', {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.text())
+            .then(data => {
+                // Manejar la respuesta del servidor (data)
+            })
+            .catch(error => {
+                // Manejar errores
+            });
+    });
 
 
 
