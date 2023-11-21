35686aa60886f29be834f14356505c8d531cb08d
Yesid <yesidrodriguez305@gmail.com>
2023-11-21 09:52:11 -0500

    Add function to get the list of favorite services

--- model/favorite.model.php
+++ model/favorite.model.php
@@ -31,4 +31,19 @@ class FavoriteServiceModel {
 
     return array('status' => 200, 'message' => 'Servicio favorito obtenido', 'data' => $service);
   }
+
+  public static function readFavoriteServices($idCustomer) {
+    try {
+      $query = "SELECT services.id_service, services.name_service, services.description_service, services.price, services.location, services.city, services.country, services.amount_people, services.characteristics, services.id_type_service, services.id_supplier, images_services.id_image, images_services.url_image FROM services INNER JOIN images_services ON services.id_service = images_services.id_service INNER JOIN favorites ON services.id_service = favorites.id_service WHERE favorites.id_customer = ?";
+      $result = Connection::connect()->prepare($query);
+      $result->bindParam(1, $idCustomer, PDO::PARAM_INT);
+      $result->execute();
+      $services = $result->fetchAll();
+      $result = null;
+
+      return array("codigo" => "200", "mensaje" => "ok", "data" => $services);
+    } catch (Exception $e) {
+      return array("codigo" => "500", "mensaje" => $e->getMessage());
+    }
+  }
 }
