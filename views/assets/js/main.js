349e7a6b794db987b9a46173b79323b859a780e0
Yesid <yesidrodriguez305@gmail.com>
2023-11-20 21:33:30 -0500

    feat(app): Add functionality to view details of a service

--- views/assets/js/main.js
+++ views/assets/js/main.js
@@ -1,10 +1,6 @@
 // const loginFormButton = document.getElementById('log-in-form-button');
 // loginFormButton.addEventListener('click', method());
 
-var tabla = null;
-ShowServices(); 
-
-ShowServices();
 async function fetchData(url, method, data) {
   const response = await fetch(url, {
     method,
@@ -112,30 +108,112 @@ async function loginUser() {
   }
 }
 
+async function getInfoService(idService) {
+  const data = new FormData();
+  data.set('action', 'read');
+  data.set('idService', idService);
 
+  const url = `./control/info-service.control.php`;
+  const method = 'POST';
 
-
-
-function ShowServices() {
-
-  // se crea la peticion para consultar la informacion
-  var objData = new FormData();
-  objData.append("ShowServices", "OK");
-
-  fetch('../../control/info-service.control.php', {
-      method: 'POST',
-      body: objData
-      // este es para capturar el error
-  }).then(response => response.json()).catch(error => {
-      console.log('error: ', error);
-      // este  captura la respuesta si es positiva
-  }).then(response => {
-      NameService(response)
-  });
-
+  try {
+    const response = await fetchData(url, method, data);
+    if (response.status === 200) {
+      showInfoService(response.data);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
+function showInfoService(data) {
+  // gallery
+  const infoServiceGallery = document.createElement('div');
+  infoServiceGallery.classList.add('info-service__gallery');
 
-function NameService(response) {
-  
+  for (let i = 0; i < 4; i++) {
+    const infoServiceGalleryImg = document.createElement('img');
+    infoServiceGalleryImg.src = data.images[i].url_image;
+    infoServiceGalleryImg.alt = '';
+
+    infoServiceGallery.appendChild(infoServiceGalleryImg);
+  }
+
+  // details
+  const infoServiceDetails = document.createElement('div');
+  infoServiceDetails.classList.add('info-service__details');
+
+  // sub container
+  const infoServiceDetailsSubContainer = document.createElement('div');
+
+  // header
+  const infoServiceDetailsHeader = document.createElement('div');
+  infoServiceDetailsHeader.classList.add('info-service__details__header');
+
+  const infoServiceDetailsHeaderTitle = document.createElement('div');
+  infoServiceDetailsHeaderTitle.classList.add(
+    'info-service__details__header__title'
+  );
+
+  const infoServiceDetailsHeaderTitleH2 = document.createElement('h2');
+  infoServiceDetailsHeaderTitleH2.textContent = data.name_service;
+
+  const infoServiceDetailsHeaderTitleP = document.createElement('p');
+  infoServiceDetailsHeaderTitleP.textContent = `${data.location} | ${data.city} - ${data.country}`;
+
+  infoServiceDetailsHeaderTitle.appendChild(infoServiceDetailsHeaderTitleH2);
+  infoServiceDetailsHeaderTitle.appendChild(infoServiceDetailsHeaderTitleP);
+
+  // rating
+  const infoServiceDetailsHeaderRating = document.createElement('div');
+  infoServiceDetailsHeaderRating.classList.add(
+    'info-service__details__header__rating'
+  );
+
+  const infoServiceDetailsHeaderRatingP = document.createElement('p');
+  infoServiceDetailsHeaderRatingP.textContent = `4.5⭐`;
+
+  infoServiceDetailsHeaderRating.appendChild(infoServiceDetailsHeaderRatingP);
+
+  infoServiceDetailsHeader.appendChild(infoServiceDetailsHeaderTitle);
+  infoServiceDetailsHeader.appendChild(infoServiceDetailsHeaderRating);
+
+  // body
+  const infoServiceDetailsBody = document.createElement('div');
+  infoServiceDetailsBody.classList.add('info-service__details__body');
+
+  const infoServiceDetailsBodyP = document.createElement('p');
+  infoServiceDetailsBodyP.textContent = data.description_service;
+
+  infoServiceDetailsBody.appendChild(infoServiceDetailsBodyP);
+
+  infoServiceDetailsSubContainer.appendChild(infoServiceDetailsHeader);
+  infoServiceDetailsSubContainer.appendChild(infoServiceDetailsBody);
+
+  // price card
+  const infoServiceDetailsPriceCard = document.createElement('div');
+  infoServiceDetailsPriceCard.classList.add(
+    'info-service__details__price-card'
+  );
+
+  const infoServiceDetailsPriceCardSpan = document.createElement('span');
+  infoServiceDetailsPriceCardSpan.textContent = `${data.price}€ / 24h`;
+
+  const infoServiceDetailsPriceCardButton = document.createElement('button');
+  infoServiceDetailsPriceCardButton.textContent = 'Reservar';
+
+  infoServiceDetailsPriceCard.appendChild(infoServiceDetailsPriceCardSpan);
+  infoServiceDetailsPriceCard.appendChild(infoServiceDetailsPriceCardButton);
+
+  infoServiceDetails.appendChild(infoServiceDetailsSubContainer);
+  infoServiceDetails.appendChild(infoServiceDetailsPriceCard);
+
+  const infoService = document.querySelector('.info-service');
+  infoService.appendChild(infoServiceGallery);
+  infoService.appendChild(infoServiceDetails);
+}
+
+if (location.pathname.includes('/service')) {
+  const serviceId = location.search.split('?/')[1];
+  getInfoService(serviceId);
 }
