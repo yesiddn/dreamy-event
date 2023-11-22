fbf7e9765651a46fcefc4d9c9e7af68cd387d486
Yesid <yesidrodriguez305@gmail.com>
2023-11-21 20:52:22 -0500

    feat(app): Add favorite services section

--- views/modules/favorite-services-list.php
+++ views/modules/favorite-services-list.php
@@ -1,18 +1,25 @@
-<h2>Mis favoritos</h2>
-<section class="cards-container" id="cards__container">
+<section class="favorite-services">
+  <h2>Mis favoritos</h2>
+  <div class="cards-container" id="cards__container">
 
-  <!-- <a href="#" class="card">
-    <button></button>
-    <img src="./views/assets/img/img2.jpeg" alt="Imagen 2">
-    <div class="card__content">
-      <div class="card__title">
-        <h2></h2>
-        <span>★4.5</span>
+    <!-- <a href="#" class="card">
+      <button></button>
+      <img src="./views/assets/img/img2.jpeg" alt="Imagen 2">
+      <div class="card__content">
+        <div class="card__title">
+          <h2></h2>
+          <span>★4.5</span>
+        </div>
+        <p class="">Ubicación: <span></span></p>
+        <p class=""></p>
       </div>
-      <p class="">Ubicación: <span></span></p>
-      <p class=""></p>
-    </div>
-  </a> -->
+    </a> -->
 
-  <script defer src="./views/assets/js/favorites.js"></script>
+    <script defer src="./views/assets/js/favorites.js"></script>
+    <script>
+      document.addEventListener('DOMContentLoaded', () => {
+        getFavoriteServices();
+      });
+    </script>
+  </div>
 </section>
