07030901ddb54058cf8b84149247f8258ad40337
Yesid <yesidrodriguez305@gmail.com>
2023-11-22 10:49:22 -0500

    Add SupplierControl class

--- control/supplier.control.php
+++ control/supplier.control.php
@@ -7,7 +7,14 @@ class SupplierControl {
   public $rut;
   public $city;
   public $country;
+  public $image;
   public $userId;
+
+  public function addSupplier() {
+    $objRespuesta = supplierModel::mdlSupplier($this->nameCompany,$this->email,$this->phone,$this->rut,$this->city,$this->country,$this->image,$this->userId);
+    echo json_encode($objRespuesta);
+}
+
 }
 
 if($_POST['action'] == 'create') {
@@ -18,5 +25,9 @@ if($_POST['action'] == 'create') {
   $customer->rut = $_POST['rut'];
   $customer->city = $_POST['city'];
   $customer->country = $_POST['country'];
+  $customer->image = $_POST['image'];
   $customer->userId = $_POST['id_user'];
 }
+
+
+ 
