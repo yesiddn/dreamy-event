de0bbe301a0ebf0cc6d8ab0ecaabaea8c695a046
Yesid <yesidrodriguez305@gmail.com>
2023-11-22 22:11:09 -0500

    feat(server): Add createSupplier method to SupplierControl class

--- control/supplier.control.php
+++ control/supplier.control.php
@@ -1,22 +1,33 @@
 <?php
+session_start();
+include_once '../model/supplier.model.php';
 
 class SupplierControl {
-  public $nameCompany;
+  public $name;
   public $email;
   public $phone;
   public $rut;
   public $city;
   public $country;
+  public $img;
   public $userId;
+
+  public function createSupplier() {
+    $newSupplier = SupplierModel::createSupplier($this->name, $this->email, $this->phone, $this->rut, $this->city, $this->country, $this->img, $this->userId);
+
+    echo json_encode($newSupplier);
+  }
 }
 
 if($_POST['action'] == 'create') {
   $customer = new SupplierControl();
-  $customer->nameCompany = $_POST['name_company'];
+  $customer->name = $_POST['name'];
   $customer->email = $_POST['email'];
   $customer->phone = $_POST['phone'];
   $customer->rut = $_POST['rut'];
   $customer->city = $_POST['city'];
   $customer->country = $_POST['country'];
+  $customer->img = $_FILES['img'];
   $customer->userId = $_POST['id_user'];
+  $customer->createSupplier();
 }
