cb0dfe5244a8c1421ca7a82a8ab292c039f7a62c
Yesid <yesidrodriguez305@gmail.com>
2023-11-25 16:56:41 -0500

    refactor(app): Refactor event list styles and structure file

---
+++ views/modules/events.php
@@ -0,0 +1,61 @@
+<section class="my-events">
+  <h2>Mis eventos</h2>
+
+  <div class="event-list">
+    <a class="event" href="#">
+
+      <div class="event__header">
+        <div class="event__title">
+          <span>happy birthday</span>
+        </div>
+        <div class="event__header__details">
+          <span>cantidad de servicios <span class="span-color">3</span></span>
+          <span>Fecha:<span class="span-color">10/12/2023</span></span>
+        </div>
+
+      </div>
+
+      <div class="event__price">
+        <span>Total: <span class="span-color">$500.000</span></span>
+      </div>
+
+    </a>
+
+    <a class="event" href="#">
+
+      <div class="event__header">
+        <div class="event__title">
+          <span>happy birthday</span>
+        </div>
+        <div class="event__header__details">
+          <span>cantidad de servicios <span class="span-color">3</span></span>
+          <span>Fecha:<span class="span-color">10/12/2023</span></span>
+        </div>
+      </div>
+
+      <div class="event__price">
+        <span>Total: <span class="span-color">$500.000</span></span>
+      </div>
+
+    </a>
+
+    <a class="event" href="#">
+
+      <div class="event__header">
+        <div class="event__title">
+          <span>happy birthday</span>
+        </div>
+        <div class="event__header__details">
+          <span>cantidad de servicios <span class="span-color">3</span></span>
+          <span>Fecha:<span class="span-color">10/12/2023</span></span>
+        </div>
+
+      </div>
+
+      <div class="event__price">
+        <span>Total: <span class="span-color">$500.000</span></span>
+      </div>
+
+    </a>
+  </div>
+</section>
