45060f1d6f8d33e7ba3e40fc708f6d259e97b480
Yesid <yesidrodriguez305@gmail.com>
2023-11-26 17:38:55 -0500

    refactor(app): Update events module

--- views/modules/events.php
+++ views/modules/events.php
@@ -1,61 +1,24 @@
 <section class="my-events">
   <h2>Mis eventos</h2>
 
-  <div class="event-list">
-    <a class="event" href="#">
+  <div class="events-list">
+    <!-- <a class="event" href="#">
 
       <div class="event__header">
-        <div class="event__title">
-          <span>happy birthday</span>
-        </div>
+        <h3 title="Cumpleaños">Cumpleaño</h3>
+
         <div class="event__header__details">
-          <span>cantidad de servicios <span class="span-color">3</span></span>
-          <span>Fecha:<span class="span-color">10/12/2023</span></span>
-        </div>
-
-      </div>
-
-      <div class="event__price">
-        <span>Total: <span class="span-color">$500.000</span></span>
-      </div>
-
-    </a>
-
-    <a class="event" href="#">
-
-      <div class="event__header">
-        <div class="event__title">
-          <span>happy birthday</span>
-        </div>
-        <div class="event__header__details">
-          <span>cantidad de servicios <span class="span-color">3</span></span>
-          <span>Fecha:<span class="span-color">10/12/2023</span></span>
+          <p>cantidad de servicios <span class="highlight-text">3</span></p>
+          <p>Fecha:<span class="highlight-text">10/12/2023</span></p>
         </div>
       </div>
 
       <div class="event__price">
-        <span>Total: <span class="span-color">$500.000</span></span>
+        <h4>Total: <span class="highlight-text">$500.000</span></h4>
       </div>
 
-    </a>
-
-    <a class="event" href="#">
-
-      <div class="event__header">
-        <div class="event__title">
-          <span>happy birthday</span>
-        </div>
-        <div class="event__header__details">
-          <span>cantidad de servicios <span class="span-color">3</span></span>
-          <span>Fecha:<span class="span-color">10/12/2023</span></span>
-        </div>
-
-      </div>
-
-      <div class="event__price">
-        <span>Total: <span class="span-color">$500.000</span></span>
-      </div>
-
-    </a>
+    </a> -->
   </div>
+
+  <script defer src="./views/assets/js/events.js"></script>
 </section>
