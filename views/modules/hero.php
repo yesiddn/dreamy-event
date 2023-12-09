09f420fdab3be5ce883e0ffbd43bf11a5235f883
Yesid <yesidrodriguez305@gmail.com>
2023-12-08 22:54:30 -0500

    refactor(app): Update link in hero section

--- views/modules/hero.php
+++ views/modules/hero.php
@@ -2,6 +2,6 @@
   <img src="./views/assets/img/birthday-hero.jpeg" alt="Hero Image" class="hero-image">
   <div class="hero-text">
     <h2>Administra tus eventos con mayor facilidad y sin olvidar detalles</h2>
-    <a href="views/modules/new-event-form.php" target="_blank" class="hero-button">Crea un evento</a>
+    <a href="my-events" target="_blank" class="hero-button">Crea un evento</a>
   </div>
 </div>
