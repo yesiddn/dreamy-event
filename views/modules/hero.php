b2cce8aa9c6a13f32a4bc968fd1edbc5366ae7ed
Yesid <yesidrodriguez305@gmail.com>
2023-12-08 22:35:24 -0500

    refactor(app): Refactor hero module

---
+++ views/modules/hero.php
@@ -0,0 +1,7 @@
+<div class="hero-container">
+  <img src="./views/assets/img/birthday-hero.jpeg" alt="Hero Image" class="hero-image">
+  <div class="hero-text">
+    <h2>Administra tus eventos con mayor facilidad y sin olvidar detalles</h2>
+    <a href="views/modules/new-event-form.php" target="_blank" class="hero-button">Crea un evento</a>
+  </div>
+</div>
