4bab34741e5f2937f11a8115488f98d6189fee82
Yesid <yesidrodriguez305@gmail.com>
2023-12-12 08:08:50 -0500

    Fix query to get favorite services

--- model/favorite.model.php
+++ model/favorite.model.php
@@ -34,7 +34,7 @@ class FavoriteServiceModel {
 
   public static function readFavoriteServices($idCustomer) {
     try {
-      $query = "SELECT services.id_service, services.name_service, services.description_service, services.price, services.location, services.city, services.country, services.amount_people, services.characteristics, services.id_type_service, services.id_supplier, images_services.id_image, images_services.url_image FROM services INNER JOIN images_services ON services.id_service = images_services.id_service INNER JOIN favorites ON services.id_service = favorites.id_service WHERE favorites.id_customer = ?";
+      $query = "SELECT services.id_service, services.name_service, services.description_service, services.price, services.location, services.city, services.country, services.amount_people, services.characteristics, ROUND(AVG(COALESCE(comments.calificacion_comentario, 0)), 1) AS rating, services.id_type_service, services.id_supplier, images_services.id_image, images_services.url_image FROM services INNER JOIN images_services ON services.id_service = images_services.id_service LEFT JOIN comments ON comments.id_servicio = services.id_service INNER JOIN favorites ON services.id_service = favorites.id_service WHERE favorites.id_customer = ? GROUP BY comments.id_servicio;";
       $result = Connection::connect()->prepare($query);
       $result->bindParam(1, $idCustomer, PDO::PARAM_INT);
       $result->execute();
