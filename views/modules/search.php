a0548596c118fda7076e4b20789c0790a8c5d096
Yesid <yesidrodriguez305@gmail.com>
2023-12-12 07:50:05 -0500

    Refactor search page structure

--- views/modules/search.php
+++ views/modules/search.php
@@ -1,8 +1,7 @@
 <section>
+  <h1>Resultados de tu busqueda</h1>
+  <div id="searchResults">
 
-<h1>Resultados de tu busqueda</h1>
-<div id="searchResults">
-
-</div>
-<script defer src="./views/assets/js/search.js"></script>
+  </div>
+  <script defer src="./views/assets/js/search.js"></script>
 </section>
